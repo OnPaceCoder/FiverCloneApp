30215d574c40da0c9f9ce801de6d795989565d25
Priyank Vasoya <priyankvasoya7@gmail.com>
2023-07-18 22:05:34 +0530

    Review Card updated

--- client/src/components/reviews/Reviews.jsx
+++ client/src/components/reviews/Reviews.jsx
@@ -37,6 +37,20 @@ const Reviews = ({ gigId }) => {
                 : error
                     ? "Something went wrong!"
                     : data.map((review) => <Review key={review._id} review={review} />)}
+            <div className="add">
+                <h3>Add a review</h3>
+                <form action="" className="addForm" onSubmit={handleSubmit}>
+                    <input type="text" placeholder="write your opinion" />
+                    <select name="" id="">
+                        <option value={1}>1</option>
+                        <option value={2}>2</option>
+                        <option value={3}>3</option>
+                        <option value={4}>4</option>
+                        <option value={5}>5</option>
+                    </select>
+                    <button>Send</button>
+                </form>
+            </div>
         </div>
     )
 }
